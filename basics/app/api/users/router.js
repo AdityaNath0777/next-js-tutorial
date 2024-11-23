5807cabf66cfa9ec46b59231b48f966784c940a3
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-11-24 00:03:53 +0530

    #001 add next.js basics notes

---
+++ basics/app/api/users/router.js
@@ -0,0 +1,3 @@
+export async function GET(request) {
+  return new Response('Raam Raam Next.js');
+}
---
+++ basics/app/posts/loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <>
+      <div>My Loading component...</div>
+      <LoadingSkeleton />
+    </>
+  );
+};
+
+export default loading;
