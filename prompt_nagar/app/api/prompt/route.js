39537b1512703ab4bbb48ab1ab797e8d509126b4
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-12-14 00:03:16 +0530

    #026 add performance metrics in api routes for testing

--- prompt_nagar/app/api/prompt/route.js
+++ prompt_nagar/app/api/prompt/route.js
@@ -2,26 +2,32 @@ import { connectToDB } from "@utils/database";
 import { Prompt } from "@models/prompt.model";
 
 export const GET = async (req, res) => {
+  const startTime = performance.now();
   try {
     await connectToDB();
 
-    const revalidateTime = parseInt(process.env.REVALIDATE_TIME) || 1;
-
-    console.log("fetching all the prompts...");
+    // console.log("fetching all the prompts...");
     const prompts = await Prompt.find({}).populate("creator");
-    console.log("fetching complete :: last prompt", prompts[prompts.length - 1].prompt);
+    // console.log("fetching complete :: last prompt", prompts[prompts.length - 1].prompt);
     // console.log(prompts);
     // console.log("strings prompts: ", JSON.stringify(prompts));
 
     const myRes = new Response(JSON.stringify(prompts), {
       status: 200,
       headers: {
-        "Cache-Control": `public, max-age=${revalidateTime}`,
+        "Cache-Control": `no-cache`,
       },
     });
     // console.log(myRes);
     return myRes;
   } catch (error) {
     return new Response("ERROR :: Failed to fetch prompts", { status: 500 });
+  } finally {
+    const endTime = performance.now();
+    console.log(
+      `Time took to complete the execution of fetching profile posts : ${
+        endTime - startTime
+      } ms`
+    );
   }
 };
--- prompt_nagar/app/api/users/[id]/posts/route.js
+++ prompt_nagar/app/api/users/[id]/posts/route.js
@@ -2,6 +2,7 @@ import { connectToDB } from "@utils/database";
 import { Prompt } from "@models/prompt.model";
 
 export const GET = async (req, { params }) => {
+  const startTime = performance.now();
   try {
     await connectToDB();
 
@@ -16,5 +17,12 @@ export const GET = async (req, { params }) => {
   } catch (error) {
     console.error(`ERROR :: Unable to fetch the user's posts :: `, error);
     return new Response("Failed to fetch the posts", { status: 501 });
+  } finally {
+    const endTime = performance.now();
+    console.log(
+      `Time took to complete the execution of fetching profile posts : ${
+        endTime - startTime
+      } ms`
+    );
   }
 };
